4c5c0e04e95bd35e5b7e15909c7df74b971f0a75
Henry Flantrmsky Cárdenas <hhfc@hotmail.es>
2018-09-27 09:32:59 -0500

    Final

---
+++ Employees.Data/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Employees.Data
+{
+    public class Connection
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<string> getEmployees(int id = 0)
+        {
+            var uri = new Uri(string.Format("http://masglobaltestapi.azurewebsites.net/api/Employees", string.Empty));
+            var response = await client.GetAsync(uri);
+            var content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+    }
+}
